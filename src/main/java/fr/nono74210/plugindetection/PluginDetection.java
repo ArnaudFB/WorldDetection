b68c16f0ecbb356a85282796b879fb644ce7031f
Vincent Roquelaure <roquelaurevincent@gmail.com>
2023-07-12 21:15:19 +0200

    CodeReview: - Reordering methods - Removed unused variables

--- src/main/java/fr/nono74210/plugindetection/PluginDetection.java
+++ src/main/java/fr/nono74210/plugindetection/PluginDetection.java
@@ -6,35 +6,44 @@ import fr.nono74210.plugindetection.datas.PlayerCounter;
 import fr.nono74210.plugindetection.listeners.DestroyBlockListener;
 import fr.nono74210.plugindetection.timedtypes.TimedHashSet;
 import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
 import java.util.UUID;
 
 public final class PluginDetection extends JavaPlugin implements Listener {
     private static PluginDetection instance;
     private TimedHashSet<PlayerCounter> playerCounterList;
     private TimedHashSet<UUID> playerMessages;
-    public static PluginDetection getInstance() { return instance; }
+
+    private static ConsoleCommandSender log;
 
     @Override
     public void onEnable() {
         instance = this;
+        log = Bukkit.getConsoleSender();
 
         playerCounterList = new TimedHashSet<>();
         playerMessages = new TimedHashSet<>();
 
+        log.sendMessage("&ePluginDetection enabling...");
         saveDefaultConfig();
-        Bukkit.getLogger();
+
         getServer().getPluginManager().registerEvents(new DestroyBlockListener(), this);
-        Objects.requireNonNull(this.getCommand("detection")).setExecutor(new CommandReload());
-        Objects.requireNonNull(this.getCommand("detection")).setTabCompleter(new DetectionTab());
+
+        getCommand("detection").setExecutor(new CommandReload());
+        getCommand("detection").setTabCompleter(new DetectionTab());
+
+        log.sendMessage("&ePluginDetection enabled!");
     }
+
     @Override
     public void onDisable() {
-        Bukkit.getLogger();
+        log.sendMessage("&cPluginDetection disabled!");
     }
+
+    public static PluginDetection getInstance() { return instance; }
 
     public TimedHashSet<PlayerCounter> getPlayerCounter() {
         return playerCounterList;
